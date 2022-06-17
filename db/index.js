42f5b9a2fa3a7f75f9f9052c438e5b1dddc59977
dbcxx <bamideledolapo.s@gmail.com>
2022-06-17 20:24:20 +0100

    server running up to this point

--- db/index.js
+++ db/index.js
@@ -1,45 +1,23 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const { MONGO_URI } = process.env;
 
-// create the connection function
-const connectDB = () => {
-    mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useCreateIndex: trmongoose.usted,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    })
-    .then(() => {
-        console.log("Mongodb..connected ....");
+const http = require("http");
 
-    })
-    .catch((err) => {
-        console.error(err.message);
-        // exit with fail
+//const host = 'localhost';
+//const port = 8000;
+const {PORT} = process.env;
+const {HOST} = process.env;
+const port = process.env.PORT || PORT
+const host = process.env.HOST || HOST
 
-        process.exit(1)
-    })
-}
 
-//mongoose connection
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My first server!");
+};
 
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(MONGO_URI, {
-//       useNewUrlParser: true,
-//       useCreateIndex: true,
-//       useUnifiedTopology: true,
-//       useFindAndModify: false,
-//     });
 
-//     console.log("Mongodb..connected ....");
-//   } catch (err) {
-//     console.error(err.message);
-//     // exit with fail
-
-//     process.exit(1);
-//   }
-// };
-
-module.exports = connectDB;
+const server = http.createServer(requestListener);
+server.listen(port, HOST, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
